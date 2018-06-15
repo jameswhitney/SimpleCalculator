6f54249e5b70bd3279e4a71de886380d899e2824
James Whitney <jamesdrums@mac.com>
2018-06-15 10:34:08 -0400

    feat: Creates Calcultor variables and methods

--- Simple Calculator/CalculatorViewController.swift
+++ Simple Calculator/CalculatorViewController.swift
@@ -8,8 +8,24 @@
 
 import UIKit
 
-class CalculatorViewController: UIViewController {
+// MARK: Enum Modes
 
+enum modes {
+    case not_set
+    case addition
+    case substraction
+}
+
+// MARK: - CalculatorViewController: UIViewController
+
+class CalculatorViewController: UIViewController {
+    
+    // Instance variables
+    var labelString: String = "0"
+    var currentMode: modes = .not_set
+    var savedNumber: Int = 0
+    var lastButtonWasMode: Boole = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +35,16 @@ class CalculatorViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // updates text in UIlabel
+    func updateText() {
+        
+    }
+    
+    // changes mode selected by user, ie addition or subtraction
+    func changeModes(newMode: modes) {
+        
+    }
 
 }
 
